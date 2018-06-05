7c64207c07e64d3a4a3948b8ccfc4107cd33008c
bryanelidimas <bryanelidimas@gmail.com>
2018-06-05 17:03:05 -0500

    Create WithStateProps component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,21 @@
-export default function () {
-  return {}
+// When an action is dispatched to the store,
+// reducers get the action. And then,
+// reducers can modify the state with/from that action.
+
+const rootReducer = (state = {}, action) => {
+    switch(action.type) {
+      case 'APPEND_ACTION_TO_STATE':
+        return {
+          ...state,
+          action
+        }
+        break;
+      case 'FLIP_BOOL_ACTION':
+        return Object.assign({}, state, { boolAction: !state.boolAction });
+        break;
+      default:
+        return state
+    }
 }
+
+export default rootReducer

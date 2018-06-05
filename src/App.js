c101a510bf03182ee9923ec5858a13a809d1b7ab
bryanelidimas <bryanelidimas@gmail.com>
2018-06-05 14:24:32 -0500

    Create most basic component with no props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import MostBasicWithNoArguments from './components/MostBasicWithNoArguments'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App" style={{ maxWidth: '700px', margin: 'auto' }}>
+        <h1>App</h1>
+
+        <MostBasicWithNoArguments />
       </div>
     );
   }
